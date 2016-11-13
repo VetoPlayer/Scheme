adbc02bc2aa97420801eb76fde717ffed3996614
Andrew <wormxexraped@gmail.com>
2016-11-13 20:52:15 +0100

    Today's work
    Uh Yeah

--- Exams TDE.rkt
+++ Exams TDE.rkt
@@ -1,11 +1,133 @@
 #lang racket
-;;5/07/2013 function numberList taking as input a list of numbers and returns a list of pairs of numbers: yJ= xi + xJ
+;;5/07/2013 function numberList taking as input a list of numbers and returns a list of pairs of numbers: xJ=xJ yJ= xi + xJ
 ;; li: input list, second parameter: xi of the previous iteration res: result
-(define (numberList li)
-  (define (tail-numberList li xi res)
-    (if (null? li)
-        res
-       (tail-numberList (cdr li) (+ xi (car li)) (list res (cons (car li) (+ xi (car li)))))))
-  (tail-numberList li 0 '()))
 
-;;
+;; Remake: It's way easier than it seems
+
+(define (numberList lst state)
+  (if (null? lst)
+      '()
+      (cons (cons (car lst) (+ (car lst) state)) (numberList (cdr lst) (+ state (car lst))))))
+
+;; 24/07/2013
+;; Define a double linked list that has 3 components: 1° prev pointer 2° datum 3° next pointer.
+;; A well made list has its first element's prev and its last element's next set to NIL
+
+;; Define DList Datatype
+(struct Dlist (
+  (prev #:mutable)
+  (datum #:mutable)
+  (next #:mutable)
+  ))
+;; Yes, prev and next are pointers
+
+;; Nil Object
+;; HERE Nil is implicitly defined also as a constructor!
+;; If you want to build up a Nil element for a Dlist you shoul invoke the constructor  instead of doing like (Dlist #f #f #f)
+;; because in that case the eq? operator for Nil will evaluate false
+(define Nil (Dlist #f #f #f))
+(define (Nil? x) (eq? x Nil))
+
+;; Remark: the whole list is passed as its first node!! This has the links to all the nodes within the list
+
+
+;; Dcar
+(define (Dcar node)
+  (if (Nil? node)
+      (error "Dcar of Nil")
+      (Dlist-datum node)))
+
+;; Dcdr
+(define (Dcdr node)
+  (if (Nil? node)
+      (error "Dcdr of a Nil, you bastard")
+      (let ((next (Dlist-next node)))
+        (Dlist Nil (Dlist-datum next) (Dlist-next next)))))
+
+;; Dcons
+(define (Dcons item node)
+  (if (Nil? node)
+      (Dlist Nil item Nil)
+      (let* ((newnode (Dlist Nil item (Dlist-datum node)))
+             (newcar (Dlist Nil (Dlist-datum node) (Dlist-next node))))
+        (set-Dlist-prev! newcar newnode)) ;; Here you set up the pointer, you need to fully create it in the let* assignment
+
+      ))
+
+;; Dlist? holding if both elements are equal
+;; Making it recursive it the smart thing to do ;)
+(define (Dlist=? node1 node2)
+  (cond ((and (Nil? node1)(Nil? node2)) #t)
+        ((and (Nil? node1)(Nil? node2)) #f)
+        (else
+         (equal? (Dlist-datum node1) (Dlist-datum node2))
+         (Dlist=? (Dcdr node1) (Dcdr node2 )))))
+
+
+;; 05/09/2013 Parent-pointer Trees
+; You don't need a struct, you already have a vector of pairs for representing everything
+;(struct PPT ((parent #:mutable)
+;            (data #:mutable)))
+
+;; define the operation find-root, to obtain the root of the tree containing the given node
+;; Remark: node is a pair
+(define (find-root vect node)
+  (if (Nothing? (car node))
+       node
+      (find-root (vect) (vector-ref vect (cdr node)))))
+
+(define (Nothing? var)
+  (eq? #\? var))
+
+;; define union! that takes 2 nodes and, if they belong to different trees, merges the 2 trees making the first root as a parent of the second one
+;; remark: obviously it has side effects!
+(define (union! vect node1 node2)
+  (let((root1 (find-root node1)) (root2 (find-root node2)))
+    (if (eq? root1 root2)
+        (let ((root2parent (car root2)))
+        (set! root2parent root1)) ;; To fix
+        (display "The 2 selected nodes belong to the same tree"))))
+
+;; REDO with indexes
+
+;; 19/09/2013
+;; Closure: If you want to initialize some variables by yourself you can easily do it by a let procedure
+
+;; Closure as Objects!!
+(define (make-object lst)
+  (let ((my-lst lst))
+     (define (member? my-lst elem) ;; Tail recursive because I do want to do so :P
+      (if (null? my-lst)
+          #f
+          (if (= (car my-lst) elem)
+              #t
+              (or #f (member? (cdr my-lst) elem))
+          )
+
+
+      ))
+
+
+
+
+
+
+
+    ;; Dispatcher
+  (λ(message . args)
+    (apply (case message
+         ((member?) member? )
+         ((subsetsum?) subsetsum?)
+             (else (error "Method not available"))
+
+
+
+         ) args ))))
+
+
+
+
+
+
+
+
--- Fourth Exercitation.rkt
+++ Fourth Exercitation.rkt
@@ -20,6 +20,90 @@
               (set! pos (+ pos 1))
               v ))))) ;; Do you need to perfom things in a sequential way and you don't want to use begin??
 
+;; 3] Hash Map
+;; You initialize this closure giving it an hash table
+;; k is the key
+(define (find hm)
+  (λ(k)
+    (hash-ref hm k)))
+
+;; Capital-of procedure is defined over the closure: you have the closure "construction" with an already defined hash table as input.
+;; capital-of will need the same input k used in the closure on its first call
+
+(define capital-of (find
+                    (hash "Italy" "Rome" "France" "Paris" "Germany" "Berlin" "Japan" "Tokyo")))
+
+
+
+;; 4] Closures as Objects: example made in class
+(define (make-simple-object)
+  (let ((my-var 0)) ;; Inside the let you also define all methods!
+
+    ;; Methods
+    (define (my-add x)
+      (set! my-var (+ my-var x))
+      my-var)
+    ;;Getter
+    (define (get-my-var)
+      my-var)
+
+   ;; Display Everything
+    (define (my-display)
+      (newline)
+      (display "Hello :D My var is: ")
+      (display my-var)
+      (newline)
+
+      )
+
+    ;; In order to call the right method we use the λ function as a Dispatcher
+    ;; Remark: we are still inside the let parenthesis
+
+    (λ(message . args)
+      (apply ( case message
+                ((my-add) my-add)
+                ((my-display) my-display)
+                ((get-my-var) get-my-var)
+                (else (error "Unknown Method!!")))
+
+             args
+
+             ))
+    ;; With the apply instruction you apply the right method passing it (hopefully) the right parameters
+    ))
+;; The first time you make-your-object, then you call it passing a symbol representing the right method to be called and the parameters to be passed
+
+
+;; OK!! Let's now implement Inheritance By Delegation--> The children contains a parent instance
+
+(define (make-son)
+  (let ((parent (make-simple-object)) ;; That's inheritance!
+        (name "an object")
+        ))
+
+  (define (hello)
+    (display "Hi :PPPP"))
+  (define (my-display)
+    (display "I am the son of da king"))
+  ;; Dispatcher time:
+  (λ(message . args)
+    (case message
+      ((hello) (apply hello args))
+      ;; Overriding
+      ((my-display) (apply my-display args))
+
+      ;; Else: if i have no idea of what kind of method it's calling I ask my dad to perfrom it
+
+      (else (apply parent (cons message args)))
+
+      )
+
+  ))
+
+
+
+
+
 
 ;; Continuations!! 
 
@@ -61,6 +145,76 @@
 ;; Remark: saved-cunt before the call of test-cunt is only a variable. Then it becomes a function
 
 
+;;3] Break implemented through macros:
+
+;; lam is a passed lambda function
+
+(define-syntax for-each-b
+  (syntax-rules (break:)
+    ((_ lam lst break: sym)
+     (call/cc (λ(sym)(for-each lam lst))))))
+
+
+;; 4] Coroutines with continuations:
+
+;; We define a queue as a global variable
+(define *queue* '())
+
+(define (empty-queue?)
+  (null? *queue*))
+
+(define (enqueue elem)
+  (set! *queue* (cons elem *queue*)))
+
+;; A.K.A. POP, it returns the last element 
+(define (dequeue)
+  (let ((x (car *queue*)))
+  (set! *queue* (cdr *queue*))
+    x))
+
+;; Fork definition: it duplicates a procedure
+(define (fork funct)
+  (call/cc (λ(k)(enqueue k)(funct))))
+;; Yield
+(define (yield)
+  (call/cc (λ(k)(enqueue k)(dequeue))))
+
+;;C-Exit
+
+(define (c-exit)
+  (if (empty-queue?)
+      (exit)
+      ((dequeue))))
+;; Ok let's see how it does work
+
+(define (do-stuff-n-print str max)
+  (let loop ((n 0))
+    (display str)
+    (display " ")
+    (display n)
+    (newline)
+    (yield)
+    (if (< n max)
+        (loop (+ 1 n))
+        (c-exit))))
+
+(define (main)
+  (begin
+    (fork (do-stuff-n-print "This is A" 3))
+    (fork (do-stuff-n-print "This is B" 4))
+    (displayln "End")
+    (c-exit)))
+
+
+
+
+
+
+
+
+
+
+
 
 ;; Interesting things: how to build up a λ function step by step
 ;; 1] How to force foldr to be tail-recursive
@@ -79,6 +233,9 @@
 ;;2]out = Identity (f e1 x) ---> (f 1 x)
 ;; 3] (f 1 (f 2 x)))
 ;; This thing here is a Big, Big closure!!
+
+
+
 
 
 
